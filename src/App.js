a336f4b4435ca952b636bdc84205b7c2e31e520a
Zegaru <paul.cardenas.0002@mail.com>
2021-12-10 00:01:24 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Aprende más
+          Aprende mucho más
         </a>
       </header>
     </div>
